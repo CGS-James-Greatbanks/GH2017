b7bc2299e68ba3a01462fbc55ccd9d3c887fe23a
sanuki <applerito@gmail.com>
2017-07-30 12:36:10 +1000

    4th major commit

--- canberraMapFunction.js
+++ canberraMapFunction.js
@@ -1,17 +1,26 @@
 //Canberra map functions:
 var regions = ["northCanberra", "southCanberra", "woden", "belconnen", "westonCreek", "Tuggeranong", "Gungahlin"];
-var regionCoords = [{x: 0, y: 0, z: }, ]
+var regionCoords = [{x: 0, y: 0, z: -1}, {x: 0, y: 0, z: 0}, {x: -1, y: 0, z: 0}, {x: -1, y: 0, z: -1.2}, {x: -1.5, y: 0, z: 0}, {x: 0, y: 0, z: 2}, {x: 0, y: 0, z: -2}]
 var regionObjects = []
-var mapArea = document.querySelector('#mapArea')
 
 function mapLight(dataType, value) {
 	
 }
 
+
+
+
+
+
 function createMap() {
+	var mapArea = document.querySelector('#mapArea');
 	for (var i=0;i<regions.length;i++) {
 		var mapRegion = document.createElement("a-box");
-		mapRegion.setAttribute("material": {color: "#fe1f1f"});
+		mapRegion.setAttribute("material", {color: "#42f450"});
+		mapRegion.setAttribute("position", regionCoords[i]);
+		mapRegion.id = regions[i];
+		mapRegion.className = "object";
+		mapArea.appendChild(mapRegion);
 	}
 }
 
--- main.js
+++ main.js
@@ -1,13 +1,56 @@
+// Global variable storage
+
+var aframeIsDisplayed = false;
+var isViewingDataOptions = false;
+var selectedData = false; // not bool permanantly
+var yearOptions = [];
+var categories = [];
+
+
+
+// Window onload
+
 $(window).on("load", function() {
-    
-	// this func is called when the HTML is loaded & ready to interact with
-	document.getElementById('dataDropdown').style.display = 'none';
-	document.getElementById('aFrame').style.display = 'none';
-	$("#loader").fadeOut();
-	document.getElementById('mainIntro').style.zIndex = '10';
-	document.getElementById('mainIntro').style.animationName = 'slideIn';
+    // jQuery window onload function
 	
+	
+	// hide dropdown menu programatically
+	document.getElementById('dataDropdown').style.display = 'none';
+	
+	// hide the aframe div and all of it's assets
+	document.getElementById('aFrame').style.display = 'none';
+	
+	// fade out the loader div
+	$("#loader").fadeOut();
+	
+	// if the device is not a mobile or is landscape
+	if (screen.width > 736 || screen.width > screen.height) {
+		
+		// bring the mainIntro div forward
+		document.getElementById('mainIntro').style.zIndex = '10';
+		
+		// begin the slide in animation
+		document.getElementById('mainIntro').style.animationName = 'slideIn';
+	}
 });
+
+
+
+
+// create event for resize of media screen (mobile orientation change)
+var supportsOrientationChange = "onorientationchange" in window,
+    orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+window.addEventListener(orientationEvent, function() {
+    if (screen.height <= 736 && screen.width > screen.height) {
+		document.getElementById('mainIntro').style.zIndex = '10';
+	}
+	if (screen.height <= 736 && screen.height > screen.width) {
+		document.getElementById('mainIntro').style.zIndex = '-1';
+	}
+}, false);
+
+
 
 
 function changeDataset() {
@@ -37,7 +80,7 @@ function selectData(dataset, datasetName) {
 		categories = ["per-person", "per 100 people"];
 	} else if (selectedData == 'budget') {
 		yearOptions = [2018,2019,2020,2021];
-		categories = ["overall", "sector"];
+		categories = ["overall", "support", "services", "jobs", "construction", "care", "schools"];
 	}
 	
 	for (var i = 0; i < yearOptions.length; i++) {
@@ -70,111 +113,22 @@ function dataButtonPress() {
 		category = 0;
 	} else if (category == 'per 100 people') {
 		category = 1;
-	} 
+	}
 	var year = document.getElementById('year').value;
-
 	
 	// ================================== CALLING OF THE SHOWMETHEDATA FUNC =================================== /
-		 
-	showMeTheData(selectedData, year, category);
-
-	
+	determineOutputMethod(year, category)
 }
 
-function makeGreenBorder(amount) {
-	var element = document.getElementById('selectedDataset');
-	if (amount == 255) {
-		removeGreenBorder(255);
-		return;
+function determineOutputMethod(year, category) {
+	if (selectedData == "petrol") {
+		showMeTheData(selectedData, year, category);
+	} else {
+		
+		 handleBudget(year, category);
 	}
-	element.style.borderColor = 'rgb(0, ' + amount + ', 0)';
-	amount++;
-	setTimeout(function() {
-		makeGreenBorder(amount);
-	},1);
 }
 
-
-
-
-
-
-function removeGreenBorder(amount) {
-	var element = document.getElementById('selectedDataset');
-	if (amount == 0) {
-		return;
-	}
-	element.style.borderColor = 'rgb(0, ' + amount + ', 0)';
-	amount--;
-	setTimeout(function() {
-		removeGreenBorder(amount);
-	},1);
-}
-
-
-
-
-
-
-
-
-// Roan's_stuff_above
-
-// Global variable storage
-
-var aframeIsDisplayed = false;
-var isViewingDataOptions = false;
-var selectedData = false; // not bool permanantly
-var yearOptions = [];
-var categories = [];
-
-//Handle budget data
-
-function handleBudget() {
-	createBudgetFilePath()
-}
-
-function createBudgetFilePath() {
-	var year = document.getElementById('year').innerHTML
-	var path = "data/state/financial-by-year" + year.toString()
-	
-}
-
-// Function to remove all objects from A-Frame
-
-function removeObjects(){
-	 var objects = document.querySelectorAll(".object");
-	 var outputArea = document.querySelector('#outputArea');
-	 for(var i = 0; i < objects.length; i++){
-		  outputArea.removeChild(objects[i]);
-	 }
-}
-
-
-
-//Fetchs data from csv file
-function returnData(data){
-	 if (data == "petrol"){
-		  var xmlHttp = new XMLHttpRequest();
-    		xmlHttp.open( "GET", "data/national/petroleum_products_per_year.csv", false );
-    		xmlHttp.send( null );
-    		return xmlHttp.responseText;
-	 	} else if (data == "vehicles") {
-		  var xmlHttp = new XMLHttpRequest();
-    		xmlHttp.open( "GET", "data/national/vehicles_census_data.csv", false );
-    		xmlHttp.send( null );
-    		return xmlHttp.responseText;
-	 } else if (data == "pop") {
-		  var xmlHttp = new XMLHttpRequest();
-    		xmlHttp.open( "GET", "data/national/historical_population_data_national.csv", false);
-    		xmlHttp.send( null );
-    		return xmlHttp.responseText;
-	 }
-}
-
-
-
-//Splits data into
 function showMeTheData(data, year, category) {
 	
 	 aframe();
@@ -219,20 +173,173 @@ function showMeTheData(data, year, category) {
 		  startAnimBarrel(amount, ["This is an oil barrel", "Every year, 100 people use this much oil (" + amount.toString() + " barrels)."]);
 	 }
 	 
+}
 
+function makeGreenBorder(amount) {
+	var element = document.getElementById('selectedDataset');
+	if (amount == 255) {
+		removeGreenBorder(255);
+		return;
+	}
+	element.style.borderColor = 'rgb(0, ' + amount + ', 0);'+
+	amount++;
+	setTimeout(function() {
+		makeGreenBorder(amount);
+	},1);
+}
+
+function removeGreenBorder(amount) {
+	var element = document.getElementById('selectedDataset');
+	if (amount == 0) {
+		return;
+	}
+	element.style.borderColor = 'rgb(0, ' + amount + ', 0)';
+	amount--;
+	setTimeout(function() {
+		removeGreenBorder(amount);
+	},1);
+}
+
+//Handle budget data
+
+function handleBudget(year, category) {
 	 
 	 
+		 var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", "data/state/financial-by-year/" + year.toString() + ".csv", false);
+		xmlHttp.send(null);
+		budgetSplitByLine(xmlHttp.responseText, category);
 
+		 
+
+}
+
+function budgetSplitByLine(responseText, category) {
+	var split = responseText.split("\n");
+	var budgetDict = {};
+	for (var i = 0; i < split.length; i++){
+		 var xtraSplit = split[i].split(",");
+		 budgetDict[xtraSplit[0]] = xtraSplit[1]
+	}
+	
+	 if (category == "overall"){
+		  category = "total";
+	 }
+	 
+	var amount = budgetDict[category]
+	
+	console.log(amount);
+	
+	amount /= 650000;
+	
+	startAnimHouses(amount, ["One standard house is worth $650,000.", "This is how many houses you can buy with the " + category + " budget (" + parseInt(amount).toString() + " houses)."])
+	 
+	 
+}
+
+
+function startAnimHouses(amount, textArray) {
+	 
+	aframe();
+	removeObjects();
+	 
+	var outputArea = document.querySelector('#outputArea');
+	var textArea = document.querySelector("a-text");
+   
+	var object = document.createElement("a-plane");
+	object.setAttribute("material", {color: "#edb350"});
+	object.setAttribute("position", {x: 0, y: 0.005, z: -97});
+	object.setAttribute("rotation", {x: -90, y: 0, z: 0});
+	object.setAttribute("geometry", {width: 2.5, height: 200});
+	object.className = "object";
+	outputArea.appendChild(object);
+	 
+	textArea.setAttribute("text", {value: textArray[0]})
+	textArray.shift()
+
+	setTimeout(function() {houseUpdate(amount, textArray)}, 3000);
+}
+
+function houseUpdate(amount, textArray){
+	 var textArea = document.querySelector("a-text");
+	 textArea.setAttribute("text", {value: textArray[0]});
+	 if(textArray.length > 1){
+		  textArray.shift()
+		  setTimeout(function() {houseUpdate(amount, textArray)}, 3000);
+	 } else {
+		  houseSpread(amount);
+	 }
+	 
+}
+
+
+function houseSpread(amount) {
+	 
+	 console.log(amount)
+	var mainScene = document.querySelector('a-scene');
+	var outputArea = document.querySelector('#outputArea');
+	var halfOfHouse = parseInt(amount/2);
+	var halfOfHouse2 = Math.ceil(amount/2);
+	 
+	for(var i = 0; i < halfOfHouse; i++){
+		 var object = document.createElement("a-obj-model");
+		 object.setAttribute("obj-model", {obj: "#house"});
+		 object.setAttribute("material", {color: "#b3ff9e"});
+		 object.setAttribute("position", {x: -4, y: 0, z: -4.5*i});
+		 object.setAttribute("rotation", {x: 0, y: -90, z: 0})
+		 object.setAttribute("scale", {x: 2, y: 2, z: 2});
+		 outputArea.appendChild(object);
+	}
+	 
+	for(var i = 0; i < halfOfHouse2; i++){
+		 var object = document.createElement("a-obj-model");
+		 object.setAttribute("obj-model", {obj: "#house"});
+		 object.setAttribute("material", {color: "#b3ff9e"});
+		 object.setAttribute("position", {x: 4, y: 0, z: -4.5*i});
+		 object.setAttribute("rotation", {x: 0, y: 90, z: 0})
+		 object.setAttribute("scale", {x: 2, y: 2, z: 2});
+		 outputArea.appendChild(object);
+	}
+	 
+	 
 }
 
 
 
 
+// Function to remove all objects from A-Frame
+function removeObjects(){
+	 var objects = document.querySelectorAll(".object");
+	 var outputArea = document.querySelector('#outputArea');
+	 for(var i = 0; i < objects.length; i++){
+		  outputArea.removeChild(objects[i]);
+	 }
+}
 
 
 
+//Fetchs data from csv file
+function returnData(data){
+	 if (data == "petrol"){
+		  var xmlHttp = new XMLHttpRequest();
+    		xmlHttp.open( "GET", "data/national/petroleum_products_per_year.csv", false );
+    		xmlHttp.send( null );
+    		return xmlHttp.responseText;
+	 	} else if (data == "vehicles") {
+		  var xmlHttp = new XMLHttpRequest();
+    		xmlHttp.open( "GET", "data/national/vehicles_census_data.csv", false );
+    		xmlHttp.send( null );
+    		return xmlHttp.responseText;
+	 } else if (data == "pop") {
+		  var xmlHttp = new XMLHttpRequest();
+    		xmlHttp.open( "GET", "data/national/historical_population_data_national.csv", false);
+    		xmlHttp.send( null );
+    		return xmlHttp.responseText;
+	 }
+}
 
-	
+
+
 
 
 function startAnimBarrel(amount, textArray) {
@@ -251,11 +358,6 @@ function startAnimBarrel(amount, textArray) {
 	setTimeout(function() {updateText(amount, textArray)}, 3000);
 }
 
-
-
-
-
-
 function updateText(amount, textArray) {
 	 
 	 var textArea = document.querySelector("a-text");
@@ -263,18 +365,12 @@ function updateText(amount, textArray) {
 	 textArea.setAttribute("text", {value: textArray[0]});
 	 if(textArray.length > 1){
 		  textArray.shift()
-		  setTimeout(function() {updateText(amount, textArray)}, 3000);
+		  setTimeout(function() {updateext(amount, textArray)}, 3000);
 	 } else {
 		  spread(amount);
 	 }
 	 
 }
-
-
-
-
-
-
 
 function spread(amount){
 	//TODO: get type of object to show, and the text to show.
@@ -349,36 +445,18 @@ function spread(amount){
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function aframe() {
 	a=document.getElementById("aFrame");
 	introBack=document.getElementById("introScene");
-	button=document.getElementById("aframeButton")
+	//button=document.getElementById("aframeButton")
 	if (aframeIsDisplayed) {
 		a.style.display = "none";
 		introBack.style.display = "block";
-		button.innerHTML = "activate A-Frame";
+		//button.innerHTML = "activate A-Frame";
 	} else {
 		a.style.display = "block";
 		introBack.style.display = "none";
-		button.innerHTML = "deactivate A-Frame"; 
+		//button.innerHTML = "deactivate A-Frame"; 
 	}
 	aframeIsDisplayed = !aframeIsDisplayed;
 }
